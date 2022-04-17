dd72176bb2b1e39195647761f3f54325ed972364
Ariel I. Diaz <arielivandiaz@gmail.com>
2022-04-17 00:28:25 -0300

    changed to mysql

--- app/services/dbConnection.js
+++ app/services/dbConnection.js
@@ -1,9 +1,8 @@
 const Sequelize = require('sequelize');
 
 const sequelize  = new Sequelize(process.env.DB_NAME, process.env.DB_USER,process.env.DB_PASSWORD, {
-    host:process.env.DB_ADDRESS,
-    port: process.env.DB_PORT,    
-    dialect: "postgres",
+    host:process.env.DB_ADDRESS,    
+    dialect: "mysql",
     pool: {
         max: 5,
         min: 0,
